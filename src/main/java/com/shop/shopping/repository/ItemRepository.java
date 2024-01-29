f9eea34f13de15989d00f5b8614f3a2569dc5b5c
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-29 10:43:42 +0900

    feat ADD: ItemRepository에 대한 테스트 구현
    - 새로운 상품이 저장될 때 정확한 속성들이 설정되는지 검증하는 단위 테스트 구현.
    - findByItemNm을 사용하여 주어진 상품명으로 상품을 조회할 때 정확한 결과가 반환되는지 확인하는 테스트 추가.
    - 테스트 데이터 설정을 위한 createItem 및 createItemList 헬퍼 메소드 추가.
    - ItemRepository의 상품 생성 및 조회 기능에 대한 철저한 테스트를 보장.

--- src/main/java/com/shop/shopping/repository/ItemRepository.java
+++ src/main/java/com/shop/shopping/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package com.shop.shopping.repository;
 import com.shop.shopping.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByItemNm(String itemNm);
 }
--- src/test/java/com/shop/shopping/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/shopping/repository/ItemRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.cglib.core.Local;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,7 +32,7 @@ class ItemRepositoryTest {
         String itemDetail = "테스트 상품 상세 설명";
         ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
         int itemStockNumber = 100;
-        Item item = createTestItem(itemNm, price, itemDetail, ItemSellStatus.SELL, itemStockNumber, now, now);
+        Item item = createItem(itemNm, price, itemDetail, ItemSellStatus.SELL, itemStockNumber, now, now);
 
         // When
         Item savedItem = itemRepository.save(item);
@@ -45,15 +47,52 @@ class ItemRepositoryTest {
         assertTrue(savedItem.getRegTime().isEqual(now) && savedItem.getUpdateTime().isEqual(now), "상품 등록 시간과 수정 시간이 올바르게 저장되어야 합니다.");
     }
 
-    private Item createTestItem(String itemNm, int price, String itemDetail, ItemSellStatus itemSellStatus, int stockNumber, LocalDateTime regTime, LocalDateTime updateTime) {
-        Item item = new Item();
-        item.setItemNm(itemNm);
-        item.setPrice(price);
-        item.setItemDetail(itemDetail);
-        item.setItemSellStatus(itemSellStatus);
-        item.setStockNumber(stockNumber);
-        item.setRegTime(regTime);
-        item.setUpdateTime(updateTime);
-        return item;
+    @Test
+    @DisplayName("주어진 상품명으로 상품 조회 시 정확한 결과 반환")
+    public void whenSearchByItemNm_thenCorrectItemShouldBeReturned() {
+        // Given
+        LocalDateTime now = LocalDateTime.now();
+        String itemNm = "테스트 상품";
+        int price = 10000;
+        String itemDetail = "테스트 상품 상세 설명";
+        ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
+        int itemStockNumber = 100;
+        this.createItemList(itemNm, price, itemDetail, ItemSellStatus.SELL, itemStockNumber, now, now);
+
+        // When
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+
+        // Then
+        assertNotNull(itemList, "조회된 상품 리스트는 null이 아니어야 합니다.");
+        assertFalse(itemList.isEmpty(), "조회된 상품 리스트는 비어있지 않아야 합니다.");
+        assertEquals(1, itemList.size(), "정확히 하나의 상품이 조회되어야 합니다.");
+        Item item = itemList.get(0);
+        assertEquals("테스트 상품1", item.getItemNm(), "조회된 상품의 이름이 정확해야 합니다.");
+    }
+
+
+    private Item createItem(String itemNm, int price, String itemDetail, ItemSellStatus itemSellStatus, int stockNumber, LocalDateTime regTime, LocalDateTime updateTime) {
+            Item item = new Item();
+            item.setItemNm(itemNm);
+            item.setPrice(price);
+            item.setItemDetail(itemDetail);
+            item.setItemSellStatus(itemSellStatus);
+            item.setStockNumber(stockNumber);
+            item.setRegTime(regTime);
+            item.setUpdateTime(updateTime);
+            return item;
+    }
+    private void createItemList(String itemNm, int price, String itemDetail, ItemSellStatus itemSellStatus, int stockNumber, LocalDateTime regTime, LocalDateTime updateTime) {
+        for (int i=1; i<=10; i++){
+            Item item = new Item();
+            item.setItemNm(itemNm+i);
+            item.setPrice(price+i);
+            item.setItemDetail(itemDetail+i);
+            item.setItemSellStatus(itemSellStatus);
+            item.setStockNumber(stockNumber+i);
+            item.setRegTime(regTime);
+            item.setUpdateTime(updateTime);
+            itemRepository.save(item);
+        }
     }
 }
